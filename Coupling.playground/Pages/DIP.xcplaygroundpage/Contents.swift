d12ab7a87cabbe6d63f0deb020dc26e91d2e76e7
EslamAbotaleb <eslamabotaleb07@gmail.com>
2025-01-22 09:41:38 +0200

    add DIP

---
+++ Coupling.playground/Pages/DIP.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: [Previous](@previous)
+
+import Foundation
+
+protocol PaymentService {
+    func processPayment()
+}
+/*
+ Payment Service maybe :-
+ 1. Paypal
+ 2. payfort
+ 3. Stripe
+ 4. Mondia Media
+ */
+
+// 2. Implement the concrete classes with descriptive names (low level)
+class PaypalPaymentService: PaymentService {
+    func processPayment() {
+        print("Processing payment through PayPal")
+    }
+}
+
+class StripePaymentService: PaymentService {
+    func processPayment() {
+        print("Processing payment through Stripe")
+    }
+}
+
+// 3. Inject the PaymentService abstraction into UserService (DI)
+class UserService {
+    private var paymentService: PaymentService
+
+    init(paymentService: PaymentService) {
+        self.paymentService = paymentService
+    }
+
+    func createUser() {
+        paymentService.processPayment()
+    }
+}
+
+// 4. Inject dependency (via DIP) when creating UserService
+let paymentService = StripePaymentService()  // Could easily swap with PaypalPaymentService
+let userService = UserService(paymentService: paymentService)
